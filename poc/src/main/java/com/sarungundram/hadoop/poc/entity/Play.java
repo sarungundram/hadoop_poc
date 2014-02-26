b07b1c2bfb3628cdfcf546affe91314399b5dc64
sarungundram <sujithmurthy@gmail.com>
2014-02-26 15:25:13 -0600

    Add play attributes

--- poc/src/main/java/com/sarungundram/hadoop/poc/entity/Play.java
+++ poc/src/main/java/com/sarungundram/hadoop/poc/entity/Play.java
@@ -2,4 +2,19 @@ package com.sarungundram.hadoop.poc.entity;
 
 public class Play {
 
+	private int period;
+	
+	private String etype;
+	
+	private String team;
+	
+	private String player;
+	
+	private int points;
+	
+	private int num;
+	
+	private int outof;
+	
+	private String result;
 }

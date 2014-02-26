e9828dee30141fa677e9cfeed8ef918c481b2b7c
sarungundram <sujithmurthy@gmail.com>
2014-02-26 16:20:29 -0600

    more changes.

--- poc/src/main/java/com/sarungundram/hadoop/poc/LBJFreeThrowPctCalculatorDriver.java
+++ poc/src/main/java/com/sarungundram/hadoop/poc/LBJFreeThrowPctCalculatorDriver.java
@@ -1,13 +1,15 @@
 package com.sarungundram.hadoop.poc;
 
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.util.Tool;
 
 public class LBJFreeThrowPctCalculatorDriver extends Configured implements Tool {
 
-	public int run(String[] arg0) throws Exception {
-		
+	public int run(String[] args) throws Exception {
+
+		Job job = new Job(getConf(), "LBJ Free Throw %");
+
 		return 0;
 	}
-
 }
--- poc/src/main/java/com/sarungundram/hadoop/poc/entity/Play.java
+++ poc/src/main/java/com/sarungundram/hadoop/poc/entity/Play.java
@@ -17,4 +17,70 @@ public class Play {
 	private int outof;
 	
 	private String result;
+
+	public int getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(int period) {
+		this.period = period;
+	}
+
+	public String getEtype() {
+		return etype;
+	}
+
+	public void setEtype(String etype) {
+		this.etype = etype;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+	public String getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(String player) {
+		this.player = player;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public int getOutof() {
+		return outof;
+	}
+
+	public void setOutof(int outof) {
+		this.outof = outof;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+	
 }
